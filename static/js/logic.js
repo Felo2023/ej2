eaf0d3d5d73c8022c9b2ffe3b5814223b2587d15
Felo2023 <134896499+Felo2023@users.noreply.github.com>
2023-09-14 20:46:28 -0600

    corrected file

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,4 @@
+function createMap(bikeStations) {
   // Create the tile layer that will be the background of our map.
   let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
